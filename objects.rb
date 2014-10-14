2cf20b22b03bb2da4d98fc388dc9b2f45ee4e808
Dvid Silva <dsilva@doctorondemand.com>
2014-10-14 00:42:21 -0700

    expanded the objects example

--- objects.rb
+++ objects.rb
@@ -1,10 +1,15 @@
 #!/usr/bin/env ruby
 
-class marker
+class Marker
   attr_accessor :brand, :color
-
   def write
-    puts "I'm a " + @color + "marker "
+    puts "I'm a " + @color + " marker  from " + @brand
   end
-
 end
+
+Marker.write
+
+marker = Marker.new
+marker.color = "Blue"
+marker.brand = "Expo"
+marker.write
